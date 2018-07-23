95427b83f88452c897b8e033d2a892868dd10d4d
jsphkm <jsphkm12@gmail.com>
2018-07-22 19:04:41 -0700

    Complete author put and delete

--- authorsRouter.js
+++ authorsRouter.js
@@ -63,4 +63,59 @@ router.post("/", (req, res) => {
 		})  
 })
 
+router.put("/:id", (req, res) => {
+	if (!(req.params.id && req.body.id && req.params.id === req.body.id)){
+		res.status(400).json({
+			error: "Request path id and the body id values must match"
+		})
+	}
+
+	const updated = {};
+	const updateableFields = ["firstName", "lastName", "userName"];
+	updateableFields.forEach(field => {
+		if (field in req.body) {
+			updated[field] = req.body[field];
+		}
+	});
+
+	Author
+		.findOne({ userName: updated.userName })
+		.then(author => {
+			if (author) {
+				const message = `Username is already taken`;
+				console.error(message);
+				return res.status(400).send(message);
+			}
+			else {
+				Author
+					.findByIdAndUpdate(req.params.id, {$set: updated}, {new: true})
+					.then(updatedAuthor => {
+						res.status(200).json({
+							id: updatedAuthor.id,
+							name: `${updatedAuthor.firstName} ${updatedAuthor.lastName}`,
+							userName: updatedAuthor.userName
+						});
+					})
+					.catch(err => res.status(500).json({message: err}));
+			}
+		})
+});
+
+router.delete('/:id', (req, res) => {
+	BlogPost
+		.remove({ author: req.params.id })
+		.then(() => {
+			Author
+				.findByIdAndRemove(req.params.id)
+				.then(() => {
+					console.log(`Deleted the author and the related blog posts, ${req.params.id}`);
+					res.status(204).json({message: "success"});
+				});
+		})
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({error: "Something went wrong"});
+		});
+});
+
 module.exports = router;
